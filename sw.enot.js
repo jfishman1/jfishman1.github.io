edd2dba8742efade65d971cc25301b0504033637
JonF <jonfishman4@gmail.com>
2023-07-28 12:12:27 -0700

    notix test 6

--- sw.enot.js
+++ sw.enot.js
@@ -1,2 +1,2 @@
-importScripts("https://notix.io/ent/current/enot.sw.min.js?r=sw");
-//importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+//importScripts("https://notix.io/ent/current/enot.sw.min.js?r=sw");
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
