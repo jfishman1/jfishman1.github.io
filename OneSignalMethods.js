9d98c0e7d77ae46f71b0086a6e1eeda7cf90a977
JonF <jonfishman4@gmail.com>
2023-08-25 10:39:02 -0700

    sw test

--- OneSignalMethods.js
+++ OneSignalMethods.js
@@ -10,7 +10,7 @@
         //appId: "380dc082-5231-4cc2-ab51-a03da5a0e4c2", // testing
         allowLocalhostAsSecureOrigin: true,
         appId: "1db1662c-7609-4a90-b0ad-15b45407d628", //main
-        serviceWorkerParam: { scope: "/push/" },
+        serviceWorkerParam: { scope: "/js/" },
         serviceWorkerPath: "push/onesignal/OneSignalSDKWorker.js",
         promptOptions: {
           slidedown: {
