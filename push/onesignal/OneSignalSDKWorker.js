b714938ec68fa3b30dc321a8375cc6996b4da31b
JonF <jonfishman4@gmail.com>
2024-10-10 18:07:30 -0700

    downgrade to v15

--- push/onesignal/OneSignalSDKWorker.js
+++ push/onesignal/OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+//v16 SDK
+//importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+//v15 SDK
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
