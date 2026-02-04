743977523cec5bf451fce3b60c4f70321153a831
JonF <jonfishman4@gmail.com>
2026-02-03 18:52:59 -0800

    move service worker

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,4 @@
+//v16 SDK
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+//v15 SDK
+//importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
--- push/onesignal/OneSignalSDKWorker.js
+++ push/onesignal/OneSignalSDKWorker.js
@@ -1,4 +1,1 @@
-//v16 SDK
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-//v15 SDK
-//importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
